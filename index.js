1a00f19b2a606a02072355fb6b517afe9fb6ef99
keko333z <keko333z@gmail.com>
2022-11-25 10:53:42 -0300

    frontend

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('build'))
 let notes = [
     {
     userId: 1,
