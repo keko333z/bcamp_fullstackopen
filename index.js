526f5fd1365d762ca77f6b9b62e725b634f7dc83
keko333z <keko333z@gmail.com>
2022-12-01 11:45:13 -0300

    mongoose back

--- index.js
+++ index.js
@@ -1,83 +1,64 @@
+require ("./mongo")
+require ("dotenv").config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const Note = require ("./models/Note")
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
-let notes = [
-    {
-    userId: 1,
-    id: 1,
-    title: "sunt aut facere repellat provident  optio reprehenderit",
-    body: "quia et suscipitsuscipit recusandae consequuntur expedita et "
-    },
-    {
-    userId: 1,
-    id: 2,
-    title: "qui est esse",
-    body: "est rerum tempore vitae debitis possimus qui neque nisi nulla"
-    },
-    {
-    userId: 1,
-    id: 3,
-    title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    body: "et iusto sed quo iure porro eius odio et labore et velit aut"
-    },
-    {
-    userId: 1,
-    id: 4,
-    title: "eum et est occaecati algo",
-    body: "ullam et saepe reiciendis quis sunt voluptatem rerum illo velit"
-    }
-]
+let notes = []
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello there!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes =>response.json(notes))
 })
+
 app.get('/info', (request, response) => {
   let resp='<p>There are '+notes.length+' notes</p><p>'+Date()+'</p>';
   response.send(resp)
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id= Number(request.params.id);
-  
- const note=notes.find(note => note.id===id)
+const id= request.params.id;
+Note.findById(id).then(note=>response.json(note))
+
+ /*const note=notes.find(note => note.id===id)
  if(!note){
   response.status(404).send( "No se encontro la nota").end()
  }
  else {
  response.json(note)
- }
+ }*/
 })
 
 app.post('/api/notes', (request, response) => {
   const note=request.body
-  const all_id=notes.map((n) => n.id)
-  const new_id=Math.max(...all_id)+1
+ // const all_id=notes.map((n) => n.id)
+ // const new_id=Math.max(...all_id)+1
   if(!note.title ||  !note.body){
     response.status(400).json({error: "No se encontro la nota"})
   }else{
-  const newNote= { userId: note.userId, id: new_id, title: note.title, body: note.body}
-  notes=[...notes, newNote]
-  response.json(notes)
+  const newNote= new Note({ userId: note.userId, title: note.title, body: note.body})
+  newNote.save().then(note =>response.json(note)) 
+  
 }})
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id= Number(request.params.id); 
+  const param_id= request.params.id;
+  Note.deleteOne({id: param_id}).then(console.log("record deleted")).catch((error)=>console.log("something went wrong, error: "+error))
+  /* const id= Number(request.params.id); 
   const note=notes.filter(note => note.id!==id)
   notes=[...note]
-  response.status(204).end()
+  response.status(204).end()*/
 
 })
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on porto: ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,30 @@
+const pass= require ("./password")
+require ("dotenv").config()
+const  mongoose = require("mongoose")
+
+const url=process.env.DB_URI
+mongoose.connect(url).then(() => console.log("connected")).catch((error)=>console.log("Something went wrong"+error))
+/*const noteSchema= new mongoose.Schema({
+    title: String,
+    body: String,
+    userId: Number
+})
+
+const Note= new mongoose.model('Note', noteSchema)*/
+/*
+const newNote= new Note({
+    title: "dfg",
+    body: "quedfdg hacdgdge",
+    userId: 5
+})
+newNote.save().then(
+    response => {console.log(response);
+    mongoose.connection.close()}
+)
+*/
+
+/*Note.find({}).then(
+    response => {console.log(response)
+    mongoose.connection.close()
+    })
+    */
