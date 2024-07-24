38f77561077ac67bf125bbb3db4b029af4eafb76
keko333z <keko333z@gmail.com>
2024-07-24 18:53:47 -0300

    for new  deploy

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = require('./app.js')
 
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
   console.log(`Server running on porto: ${PORT}`)
 })
