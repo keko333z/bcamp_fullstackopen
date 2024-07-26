77b8e2d877147cf5d182b5dfd0019b4ea1ac7ee4
keko333z <keko333z@gmail.com>
2024-07-26 11:00:03 -0300

    new style2

--- app.js
+++ app.js
@@ -144,8 +144,8 @@ const resolvers= {
 
 
 // Express 4.0
-app.use(bodyParser.json({ limit: '2mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '2mb' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 
 
